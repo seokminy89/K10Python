d753ae9eaec94ede88aa29c8f685edcc149980cb
seokminy89 <seokminy89@naver.com>
2021-12-30 09:39:37 +0900

    과제

---
+++ homework/qu6..py
@@ -0,0 +1,24 @@
+'''
+문제 6
+
+아래와 같은 패턴의 별(*)을 출력하는 프로그램을 작성해 보세요.
+*****
+ ****
+  ***
+   **
+    *
+
+print("* * * * *")
+print(" ","* * * *")
+print(" "," ","* * *")
+print(" "," "," ","* *")
+print(" "," "," "," ","*")
+
+'''
+
+for i in range(5):
+        for j in range(i):
+                print(' ', end=' ')
+        for j in range(5-i):
+                print('*', end=' ')
+        print()
